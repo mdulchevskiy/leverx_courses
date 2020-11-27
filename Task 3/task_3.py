6da8fe0fd2468c38be688d73414329e390c93e0c
M.Dulchevskiy <mdulchevskiy@gmail.com>
2020-11-27 13:02:21 +0300

    Fixed issue #5: global var problem was solved by creating 'GlobalLikeVar' class. Function 'function' added to 'GlobalLikeVar' class as 'increase' method.

--- Task 3/task_3.py
+++ Task 3/task_3.py
@@ -4,21 +4,27 @@
 from concurrent.futures.thread import ThreadPoolExecutor
 from threading import Lock
 
-a = 0
-lock = Lock()
 
+class GlobalLikeVar:
+    def __init__(self, value: int):
+        self.value = value
+        self.__lock = Lock()
 
-def function(arg):
-    global a
-    for _ in range(arg):
-        with lock:
-            a += 1
+    def increase(self, arg: int):
+        for _ in range(arg):
+            with self.__lock:
+                self.value += 1
+
+    def __repr__(self):
+        return f'{self.value}'
 
 
 def main():
+    a = GlobalLikeVar(0)
+
     with ThreadPoolExecutor(max_workers=5) as executor:
         for i in range(5):
-            executor.submit(function, 1000000)
+            executor.submit(a.increase, 1000000)
 
     print("----------------------", a)  # ???
 
